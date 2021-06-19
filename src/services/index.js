8f6860e744c96963a0ce1c49f805ac7f8a0bee35
Roberto <beto.rodriguesds@gmail.com>
2021-06-19 15:26:49 -0300

    apikey

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'https://api.rawg.io/api'})
+const api = axios.create({ baseURL: 'https://api.rawg.io/api' })
 
 export const endpoints = {
-    games: (page = 1, search = '', platforms) => `/games?page=${page}&search=${search}&${platforms}`,
-    platforms: '/platforms/lists/parents',
-    gameById: (id) => `/games/${id}`
+    games: (page = 1, search = '', platforms) => `/games?page=${page}&search=${search}&${platforms}&key=${process.env.REACT_APP_API_KEY}`,
+    platforms: `platforms/lists/parents?key=${process.env.REACT_APP_API_KEY}`,
+    gameById: (id) => `/games/${id}?key=${process.env.REACT_APP_API_KEY}`
 }
 
 export default api
